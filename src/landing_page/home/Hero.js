30b4c2ff24867a4e27cda17882fcf081140f25f2
Utkarsha Thakur <utkarshathakur2004@gmail.com>
2026-01-06 19:51:30 +0530

    Resolve merge conflicts

--- src/landing_page/home/Hero.js
+++ src/landing_page/home/Hero.js
@@ -5,7 +5,7 @@ function Hero() {
         <div className='container p-5'>
             <div className='row text-center'>
                 <img src='/media/homeHero.png' alt='Hero Image' className='mb-5' />
-            <h1 className='mt-5'>Invest in everything</h1>
+            <h1 classNmae='mt-5'>Invest in everything</h1>
             <p>Online platform to invest in stocks, ETFs,crypto and more</p>
             <button className='btn btn-primary  fs-5 nb-5' style={{width:"20%" , margin: "auto" }}>Signup Now</button>
             
--- src/landing_page/home/Pricing.js
+++ src/landing_page/home/Pricing.js
@@ -2,7 +2,28 @@ import React from 'react';
 
 function Pricing() {
     return ( 
-        <h1>Pricing</h1>
+        <div className ='container mb-5'>
+            <div className='row'>
+                <div className='col-6'>
+                    <h1 className ='mb-3 fs-2 '>Unbeatable pricing</h1>
+                    <p>We pioneered the corner of discount broking and price transparency in india.Flat fees and no hidden charges.</p>
+                    <a href='' style={{textDecoration:"none"}}>See pricing<i className="fa fa-long-arrow-right" aria-hidden="true"></i></a>
+                </div>
+                <div className='col-6'>
+                    <div className='row text-center'>
+                        <div className='col p-3 border'>
+                            <h1 className='mb-3' >₹0</h1>
+                            <p>Free equity delivery and <br/>direct mutual funds</p>
+                        </div>
+                        <div className='col p-3 border'>
+                            <h1 className='mb-3'>₹20</h1>
+                            <p>Intraday and F&O</p>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
      );
 }
 
